ce6e6ca3a214a53260b0d7ea0b8de6bf103e78fd
rsy <nathan.riley.sy@student.ateneo.edu>
2026-02-12 20:51:44 +0800

    certcommentsforsomeparts

--- M3E-Models-AmansecJimenezSy/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-AmansecJimenezSy/MyInventorySystem/MyInventoryApp/models.py
@@ -1,3 +1,20 @@
+# Louis Amansec, , Zeus Jimenez, & Nathan Riley Sy, 244311
+#  February 2026 
+
+'''
+I hereby attest to the truth of the following facts:
+
+I have not discussed the Python language code in my program with anyone
+other than my instructor or the teaching assistants assigned to this course.
+
+I have not used Python language code obtained from another student, or
+any other unauthorized source, either modified or unmodified.
+
+If any Python language code or documentation used in my program was
+obtained from another source, such as a textbook or course notes, that has been clearly noted with proper citation in the
+comments of my program.
+'''
+
 from django.db import models
 
 # Create your models here.
