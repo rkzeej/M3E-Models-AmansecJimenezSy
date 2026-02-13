50886be21d9208b01db119443e1ae5a672597d0a
rkzeej <robbie.kryzz.zeus.jimenez@student.ateneo.edu>
2026-02-13 08:13:52 +0800

    editedcertcomms

--- M3E-Models-AmansecJimenezSy/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-AmansecJimenezSy/MyInventorySystem/MyInventoryApp/models.py
@@ -1,4 +1,4 @@
-# Louis Amansec, ######; Zeus Jimenez, 242359; & Nathan Riley Sy, 244311
+# Louis Amansec, ######; Zeus Jimenez, 242359; Nathan Riley Sy, 244311
 # February 13, 2026 
 
 '''
@@ -23,23 +23,23 @@ class Supplier(models.Model):
     name = models.CharField(max_length=100) 
     city = models.CharField(max_length=100) 
     country = models.CharField(max_length=100) 
-    createdat = models.DateTimeField() 
+    created_at = models.DateTimeField() 
     
     def getName(self): 
         return self.name 
     
     def __str__(self): 
-        return f"{self.name} - {self.city}, {self.country} created at: {self.createdat}"
+        return f"{self.name} - {self.city}, {self.country} created_at: {self.created_at}"
 
 class WaterBottle(models.Model): 
-    sku = models.CharField(max_length=10, unique=True) 
+    SKU = models.CharField(max_length=10, unique=True) 
     brand = models.CharField(max_length=100) 
     cost = models.DecimalField(max_digits=10, decimal_places=2) 
     size = models.CharField(max_length=50) 
-    mouthsize = models.CharField(max_length=50) 
+    Mouth_Size = models.CharField(max_length=50) 
     color = models.CharField(max_length=50) 
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE) 
-    currentquant = models.IntegerField() 
+    supplied_by = models.ForeignKey(Supplier, on_delete=models.CASCADE) 
+    Current_Quantity = models.IntegerField() 
     
     def __str__(self): 
-        return (f"{self.sku}: {self.brand}, {self.mouthsize}, {self.size}, {self.color}, " f"supplied by {self.supplier.name}, Cost: {self.cost} : {self.currentquant}")
+        return (f"{self.SKU}: {self.brand}, {self.Mouth_Size}, {self.size}, {self.color}, " f"supplied by {self.supplied_by.name}, Cost: {self.cost} : {self.Current_Quantity}")
